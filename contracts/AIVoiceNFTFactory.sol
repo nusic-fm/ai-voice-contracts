00c50076a0f5b927b519018e94fb5bf42742d016
zeeshanhanif <zeeshanhanif@gmail.com>
2025-02-07 21:05:57 +0500

    added manager and modifier into AIVoiceNFTFactory

--- contracts/AIVoiceNFTFactory.sol
+++ contracts/AIVoiceNFTFactory.sol
@@ -2,14 +2,22 @@
 pragma solidity ^0.8.17;
 import "@openzeppelin/contracts/utils/Strings.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "./AIVoiceNFT.sol";
 
 
-contract AIVoiceNFTFactory {
+contract AIVoiceNFTFactory is Ownable{
     event AIVoiceNFTDeployed(address _contractAddress, address _owner, string _voiceName, string _name, string _symbol);
+    
+    address public manager;
+
+    modifier onlyOwnerOrManager() {
+        require((owner() == msg.sender) || (manager == msg.sender), "Caller needs to be Owner or Manager");
+        _;
+    }
 
     function deployAIVoiceNFT(string memory _voiceName, string memory _name, 
-        string memory _symbol, string memory _baseUri) external returns (address) {
+        string memory _symbol, string memory _baseUri) external onlyOwnerOrManager returns (address) {
 
         AIVoiceNFT nft = new AIVoiceNFT(_voiceName, _name, _symbol, _baseUri);
         emit AIVoiceNFTDeployed(address(nft), msg.sender, _voiceName, _name, _symbol);
